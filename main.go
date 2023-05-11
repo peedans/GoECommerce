2e748f5d9eb1a09f22ac98ce0a454bba3248dce4
P <peezaba007@gmail.com>
2023-05-11 15:02:56 +0700

    Config

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/peedans/GoEcommerce/config"
+	"os"
+)
 
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig(envPath())
+	fmt.Println(cfg)
 }
